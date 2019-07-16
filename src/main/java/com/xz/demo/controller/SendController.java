67f8b02992781e77fa86c6fd228bd4b332def0c0
xz <xzjayx@126.com>
2019-07-16 21:22:56 +0800

    日常上传

--- src/main/java/com/xz/demo/controller/SendController.java
+++ src/main/java/com/xz/demo/controller/SendController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author xiezhi
+ */
 @RestController
 @RequestMapping("/send")
 public class SendController {
