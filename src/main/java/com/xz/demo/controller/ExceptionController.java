51aef05887f37152e27c05d1c2aa3ad8eb4757b0
xz <xzjayx@126.com>
2019-07-16 21:22:07 +0800

    日常上传

--- src/main/java/com/xz/demo/controller/ExceptionController.java
+++ src/main/java/com/xz/demo/controller/ExceptionController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author xiezhi
+ */
 @RestController
 @RequestMapping("/exception")
 public class ExceptionController {
--- src/main/java/com/xz/demo/controller/LoginController.java
+++ src/main/java/com/xz/demo/controller/LoginController.java
@@ -41,7 +41,6 @@ public class LoginController {
     }
 
 
-
     @Value("${jwt.expiration}")
     private Long expiration;
 
