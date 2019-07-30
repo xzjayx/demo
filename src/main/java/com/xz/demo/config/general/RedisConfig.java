313670bef70a9cd7000ac45a38ea4f586d7a1de2
xz <xzjayx@126.com>
2019-07-30 23:48:55 +0800

    日常上传

--- src/main/java/com/xz/demo/config/general/RedisConfig.java
+++ src/main/java/com/xz/demo/config/general/RedisConfig.java
@@ -10,6 +10,9 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * @author xiezhi
+ */
 @Configuration
 public class RedisConfig {
 
