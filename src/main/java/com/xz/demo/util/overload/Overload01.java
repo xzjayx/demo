0f21407ab3926844abb5140a14fcc71c0cf57437
xiezhi <xzjayx@126.com>
2019-10-08 18:03:42 +0800

    各种test

---
+++ src/main/java/com/xz/demo/util/overload/Overload01.java
@@ -0,0 +1,16 @@
+package com.xz.demo.util.overload;
+
+/**
+ * 定义一个方法，比较两个byte ，两个short，两个int ，两个long 是否相等，使用方法重载
+ * 重载 overload ：方法名相同，参数（类型，顺序）不同 ，和返回值无关
+ * @author xz
+ */
+public class Overload01 {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+}
